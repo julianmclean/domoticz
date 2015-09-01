6d20e85b0b206213ff1623eb4ae6ade30fbcf67c
julianmclean <julian.s.mclean@gmail.com>
2015-09-01 16:18:08 +0100

    Add SonosPlugin

--- www/app/HardwareController.js
+++ www/app/HardwareController.js
@@ -45,7 +45,7 @@ define(['app'], function (app) {
             var datatimeout=$('#hardwarecontent #hardwareparamstable #combodatatimeout').val();
 
             var text = $("#hardwarecontent #hardwareparamstable #combotype option:selected").text();
-            if ((text.indexOf("TE923") >= 0)||(text.indexOf("Volcraft") >= 0)||(text.indexOf("1-Wire") >= 0)||(text.indexOf("GPIO") >= 0)||(text.indexOf("BMP085") >= 0)||(text.indexOf("Dummy") >= 0)||(text.indexOf("System Alive") >= 0)||(text.indexOf("PiFace") >= 0)||(text.indexOf("Motherboard") >= 0)||(text.indexOf("Evohome") >= 0 && text.indexOf("script") >= 0))
+            if ((text.indexOf("TE923") >= 0)||(text.indexOf("Volcraft") >= 0)||(text.indexOf("1-Wire") >= 0)||(text.indexOf("GPIO") >= 0)||(text.indexOf("BMP085") >= 0)||(text.indexOf("Dummy") >= 0)||(text.indexOf("System Alive") >= 0)||(text.indexOf("PiFace") >= 0)||(text.indexOf("Motherboard") >= 0)(text.indexOf("Sonos") >= 0)||||(text.indexOf("Evohome") >= 0 && text.indexOf("script") >= 0))
             {
                 $.ajax({
                      url: "json.htm?type=command&param=updatehardware&htype=" + hardwaretype +
@@ -407,7 +407,7 @@ define(['app'], function (app) {
                 return;
             }
 
-            if ((text.indexOf("TE923") >= 0) || (text.indexOf("Volcraft") >= 0) || (text.indexOf("1-Wire") >= 0) || (text.indexOf("BMP085") >= 0) || (text.indexOf("Dummy") >= 0) || (text.indexOf("System Alive") >= 0) || (text.indexOf("Kodi") >= 0) || (text.indexOf("PiFace") >= 0) || (text.indexOf("GPIO") >= 0) || (text.indexOf("Evohome") >= 0 && text.indexOf("script") >= 0) || (text.indexOf("Tellstick") >= 0))
+            if ((text.indexOf("TE923") >= 0) || (text.indexOf("Volcraft") >= 0) || (text.indexOf("1-Wire") >= 0) || (text.indexOf("BMP085") >= 0) || (text.indexOf("Dummy") >= 0) || (text.indexOf("System Alive") >= 0) || (text.indexOf("Kodi") >= 0) || (text.indexOf("PiFace") >= 0) || (text.indexOf("GPIO") >= 0) || (text.indexOf("Evohome") >= 0 && text.indexOf("script") >= 0) || (text.indexOf("Tellstick") >= 0)||(text.indexOf("Sonos") >= 0))
             {
                 $.ajax({
                      url: "json.htm?type=command&param=addhardware&htype=" + hardwaretype + "&port=1&name=" + encodeURIComponent(name) + "&enabled=" + bEnabled + "&datatimeout=" + datatimeout,
@@ -2873,7 +2873,7 @@ define(['app'], function (app) {
             $("#hardwarecontent #divphilipshue").hide();
             $("#hardwarecontent #divwinddelen").hide();
 
-            if ((text.indexOf("TE923") >= 0)||(text.indexOf("Volcraft") >= 0)||(text.indexOf("BMP085") >= 0)||(text.indexOf("Dummy") >= 0)||(text.indexOf("System Alive") >= 0)||(text.indexOf("PiFace") >= 0))
+            if ((text.indexOf("TE923") >= 0)||(text.indexOf("Volcraft") >= 0)||(text.indexOf("BMP085") >= 0)||(text.indexOf("Dummy") >= 0)||(text.indexOf("System Alive") >= 0)||(text.indexOf("PiFace") >= 0)||(text.indexOf("Sonos") >= 0))
             {
                 $("#hardwarecontent #divserial").hide();
                 $("#hardwarecontent #divremote").hide();
